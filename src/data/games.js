9a74119f9c13087c275646c2d8ef4c219b01c11e
doveHa <hadove02@gmail.com>
2025-06-21 18:31:50 +0900

    ChangeStructure

--- src/data/games.js
+++ src/data/games.js
@@ -4,14 +4,14 @@
         title: "Alien Uprising",
         shortDescription: "외계인과 하피가 등장하는 생존 슈팅 게임",
         description: "이 게임은 외계인의 침략 이후를 배경으로 하며...",
-        thumbnail: "/images/alien.png", // public/images 폴더에 이미지 넣기
+        thumbnail: "/images/thumbnail/alien.png",
     },
     {
         id: 2,
         title: "Harpy Rebellion",
         shortDescription: "하피 종족의 복수를 다룬 액션 RPG",
         description: "유전실험으로 태어난 하피들이 자유를 되찾기 위한 여정...",
-        thumbnail: "/images/harpy.png",
+        thumbnail: "/images/thumbnail/harpy.png",
     },
 ];
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,17 @@
-﻿import games from "../data/games";
+﻿import React from "react";
+import games from "../data/games";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
     return (
         <div className="container">
             <h1>🎮 My Game Portfolio</h1>
-            <div className="card-grid">
+            <div className="card-grid-7x3">
                 {games.map((game) => (
                     <Link to={`/games/${game.id}`} key={game.id} className="card">
                         <img src={game.thumbnail} alt={game.title} />
                         <h3>{game.title}</h3>
-                        <p>{game.shortDescription}</p>
                     </Link>
                 ))}
             </div>
